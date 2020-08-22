b16b0561aca00f092d3d60c3e7b5b41de57fc685
Milktea17 <56477487+Milktea17@users.noreply.github.com>
2020-08-22 16:25:20 +0900

    Update MyString.cpp

--- MyString.cpp
+++ MyString.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "MyString.h"
-
+#include <string>
 
 CMyString::CMyString()
 	: m_pszData(NULL)
@@ -16,18 +16,38 @@ CMyString::~CMyString()
 
 int CMyString::SetString(const char* pszParam)
 {
-	//int 
-	
-	return 0;
+	Release();
+
+	int nLength;
+
+	nLength = strlen(pszParam);
+
+	m_pszData = new char[nLength+1];
+
+	if (nLength == 0)
+		cout << "길이가 0입니다" << endl;
+
+	if (pszParam == NULL)
+		cout << "문자열이 비어있습니다" << endl;
+
+	strcpy_s(m_pszData,sizeof(char) * (nLength+1), pszParam);
+	m_nLength = nLength;
+
+	return nLength;
 }
 
 
 const char* CMyString::GetString()
 {
-	return nullptr;
+	return m_pszData;
 }
 
 
 void CMyString::Release()
 {
+	delete[] m_pszData;
+
+	m_pszData = NULL;
+	m_nLength = 0;
+	
 }
